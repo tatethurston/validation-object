52141de99bde39a6b1f4857b46f232ce6d7980ae
Tate Thurston <tatethurston@gmail.com>
2023-09-16 04:04:17 -0700

    init (#1)

--- packages/package/src/index.ts
+++ packages/package/src/index.ts
@@ -1,3 +1,125 @@
-export function foo(): number {
-  return 5;
+/**
+ * A mapping of error names to messages.
+ */
+type Errors = Record<string, string[]>;
+
+/**
+ * A validation function for a field.
+ */
+type Validator<T> = (val: T) => string | undefined;
+
+export class ValidationErrors {
+  /**
+   * A mapping of error names to messages.
+   */
+  #errors: Errors = {};
+
+  /**
+   * Adds an error to the errors map.
+   *
+   * @param {string} name - the logical group for the error
+   * @param {string} message - the message for the error
+   */
+  add(name: string, message: string): void {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    this.#errors[name] ??= [];
+    this.#errors[name].push(message);
+  }
+
+  /**
+   * Returns the errors map.
+   *
+   * @returns {Errors} Returns `{}` if there are not any errors.
+   */
+  entries(): Errors {
+    return this.#errors;
+  }
+}
+
+/**
+ * The `context` field accepts a string to specify a particular context. This allows you
+ * to only run a subset of validations for a particular context.
+ */
+export type ValidationOptions = { context: string };
+
+/**
+ * Base class that provides validation capabilities.
+ *
+ * Subclasses implement #validation.
+ */
+export abstract class ValidationObject<Validated = Record<string, never>> {
+  /**
+   * Holds any errors from calling #validate.
+   */
+  errors = new ValidationErrors();
+
+  /**
+   * An optional adapter.
+   *
+   * This will apply to any Validator used with #validates.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  adapter?: (validation: any) => Validator<any> = undefined;
+
+  /**
+   * The validations to perform. Defined in the subclass.
+   *
+   * @param {ValidationOptions} [options] - Additional options for the validation
+   * @param {string} options.context - The context for the validation. This allows you to only run a subset of validations for a particular context.
+   */
+  protected abstract validation(options?: ValidationOptions): void;
+
+  /**
+   * Runs the subclass defined #validation method and returns the object's validation status.
+   *
+   * @param {ValidationOptions} [options] - Additional options for the validation
+   * @param {string} options.context - The context for the validation. This allows you to only run a subset of validations for a particular context.
+   * @returns {boolean} Returns `true` if the object is valid.
+   */
+  valid = (options?: ValidationOptions): this is Validated & this => {
+    this.errors = new ValidationErrors();
+    this.validation(options);
+    return Object.keys(this.errors.entries()).length === 0;
+  };
+
+  /**
+   * Validate a field.
+   *
+   * @param {string} attribute - The name of the object attribute to validate.
+   * @param {Validator} validator - The validation function to apply to @param attribute.
+   * @param {string} [message] - A message override to apply when validation fails.
+   */
+  validates = <Attribute extends keyof this>(
+    attribute: Attribute,
+    validator: (val: this[typeof attribute]) => string | undefined,
+    message?: string,
+  ) => {
+    const validate = !this.adapter ? validator : this.adapter(validator);
+    const error = validate(this[attribute]);
+    if (error) {
+      this.errors.add(attribute as string, message ?? error);
+    }
+  };
+}
+
+interface R<T, Validated> {
+  new (obj: T): T & ValidationObject<Validated>;
+}
+
+export function validationObject<T, Validated = Record<string, never>>(
+  validator: (
+    obj: InstanceType<R<T, Validated>>,
+    options?: ValidationOptions,
+  ) => void,
+): R<T, Validated> {
+  return class extends ValidationObject<Validated> {
+    constructor(obj: T) {
+      super();
+      Object.assign(this, obj);
+    }
+
+    validation(opts: ValidationOptions) {
+      validator(this as unknown as InstanceType<R<T, Validated>>, opts);
+    }
+  } as R<T, Validated>;
 }
---
+++ packages/package/src/test.ts
@@ -0,0 +1,217 @@
+import {
+  ValidationErrors,
+  ValidationObject,
+  validationObject,
+} from "./index.js";
+
+describe("ValidationErrors", () => {
+  let subject: ValidationErrors;
+  beforeEach(() => {
+    subject = new ValidationErrors();
+  });
+
+  describe("#add", () => {
+    it("adds errors", () => {
+      subject.add("foo", "must be a string");
+
+      expect(subject.entries()).toEqual({
+        foo: ["must be a string"],
+      });
+    });
+
+    it("aggregates errors with the same name", () => {
+      subject.add("foo", "must be a string");
+      subject.add("foo", "must not be undefined");
+
+      expect(subject.entries()).toEqual({
+        foo: ["must be a string", "must not be undefined"],
+      });
+    });
+
+    it("seperates errors with different names", () => {
+      subject.add("foo", "must be a string");
+      subject.add("bar", "must not be undefined");
+
+      expect(subject.entries()).toEqual({
+        bar: ["must not be undefined"],
+        foo: ["must be a string"],
+      });
+    });
+  });
+
+  describe("#entries", () => {
+    it("returns the error entries", () => {
+      subject.add("foo", "must be a string");
+
+      expect(subject.entries()).toEqual({
+        foo: ["must be a string"],
+      });
+    });
+  });
+});
+
+describe("ValidationObject", () => {
+  describe("subclass", () => {
+    class Subclass extends ValidationObject {
+      name: string;
+
+      constructor(params: { name: string }) {
+        super();
+        this.name = params.name;
+      }
+
+      validation() {
+        if (this.name.length < 10) {
+          this.errors.add("name", "must be longer than 10 characters");
+        }
+      }
+    }
+
+    describe("when valid", () => {
+      const subject = new Subclass({ name: "0123456789" });
+
+      it("#valid", () => {
+        expect(subject.valid()).toEqual(true);
+      });
+
+      it("#errors.entries", () => {
+        expect(subject.errors.entries()).toEqual({});
+      });
+    });
+
+    describe("when invalid", () => {
+      const subject = new Subclass({ name: "foo" });
+
+      it("#valid", () => {
+        expect(subject.valid()).toEqual(false);
+      });
+
+      it("#errors.entries", () => {
+        expect(subject.errors.entries()).toEqual({
+          name: ["must be longer than 10 characters"],
+        });
+      });
+    });
+  });
+
+  describe("validationObject shorthand", () => {
+    interface User {
+      name: string;
+    }
+
+    const User = validationObject<User>((obj) => {
+      if (obj.name.length < 10) {
+        obj.errors.add("name", "must be longer than 10 characters");
+      }
+    });
+
+    describe("when valid", () => {
+      const subject = new User({ name: "0123456789" });
+
+      it("#valid", () => {
+        expect(subject.valid()).toEqual(true);
+      });
+
+      it("#errors.entries", () => {
+        expect(subject.errors.entries()).toEqual({});
+      });
+    });
+
+    describe("when invalid", () => {
+      const subject = new User({ name: "foo" });
+
+      it("#valid", () => {
+        expect(subject.valid()).toEqual(false);
+      });
+
+      it("#errors.entries", () => {
+        expect(subject.errors.entries()).toEqual({
+          name: ["must be longer than 10 characters"],
+        });
+      });
+    });
+  });
+
+  describe("#validates", () => {
+    describe("subclass", () => {
+      class Subclass extends ValidationObject {
+        name: string;
+
+        constructor(params: { name: string }) {
+          super();
+          this.name = params.name;
+        }
+
+        validation() {
+          this.validates("name", (attr) =>
+            attr.length < 10 ? "must be longer than 10 characters" : undefined,
+          );
+        }
+      }
+
+      describe("when valid", () => {
+        const subject = new Subclass({ name: "0123456789" });
+
+        it("#valid", () => {
+          expect(subject.valid()).toEqual(true);
+        });
+
+        it("#errors.entries", () => {
+          expect(subject.errors.entries()).toEqual({});
+        });
+      });
+
+      describe("when invalid", () => {
+        const subject = new Subclass({ name: "foo" });
+
+        it("#valid", () => {
+          expect(subject.valid()).toEqual(false);
+        });
+
+        it("#errors.entries", () => {
+          expect(subject.errors.entries()).toEqual({
+            name: ["must be longer than 10 characters"],
+          });
+        });
+      });
+    });
+
+    describe("validationObject shorthand", () => {
+      interface User {
+        name: string;
+      }
+
+      const User = validationObject<User>((obj) => {
+        obj.validates("name", (attr) =>
+          attr.length < 10 ? "must be longer than 10 characters" : undefined,
+        );
+      });
+
+      describe("when valid", () => {
+        const subject = new User({ name: "0123456789" });
+
+        it("#valid", () => {
+          expect(subject.valid()).toEqual(true);
+        });
+
+        it("#errors.entries", () => {
+          expect(subject.errors.entries()).toEqual({});
+        });
+      });
+
+      describe("when invalid", () => {
+        const subject = new User({ name: "foo" });
+
+        it("#valid", () => {
+          expect(subject.valid()).toEqual(false);
+        });
+
+        it("#errors.entries", () => {
+          expect(subject.errors.entries()).toEqual({
+            name: ["must be longer than 10 characters"],
+          });
+        });
+      });
+    });
+  });
+});
